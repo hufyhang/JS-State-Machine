54124513ce60fc0e60ed5b0f266dda699ce7a616
Feifei HANG <l.daniel.hung@gmail.com>
2014-01-28 10:27:54 +0000

    Initial commit

---
+++ example/testing.js
@@ -0,0 +1,28 @@
+#!/usr/local/bin/node
+
+var State = require('../state');
+
+var state = new State({
+    initState: 'ready',
+    states: ['ready', 'middle', 'done']
+});
+
+state.on('ready', 'enter', function () {
+    'use strict';
+    console.log('In ready now');
+}).on('ready', 'leave', function () {
+    'use strict';
+    console.log('Left ready.');
+}).on('middle', 'enter', function (name) {
+    'use strict';
+    console.log('Welcome,', name);
+}).on('done', 'enter', function () {
+    'use strict';
+    console.log('Entered done.');
+});
+
+console.log('Begin now...');
+
+state.forth('Feifei').go('ready').forth('Lanlan').forth().back('Casanova').forth();
+
+console.log('Exit now...');
---
+++ state.js
@@ -0,0 +1,108 @@
+#!/usr/local/bin/node
+
+function State(args) {
+    'use strict';
+    this.states = args.states || [];
+    this.current = args.initState || undefined;
+    this.events = {};
+}
+
+State.prototype = {
+    state: function () {
+        'use strict';
+        return this.current;
+    },
+    addStates: function (args) {
+        'use strict';
+        var index, elementIndex;
+        if (args !== undefined && args.length !== 0) {
+            for (index = 0; index !== args.length; ++index) {
+                elementIndex = this.states.indexOf(args[index]);
+                // only add those not in state list yet.
+                if (elementIndex === -1) {
+                    this.states.pop(args[index]);
+                }
+            }
+        }
+        return this;
+    },
+    removeStates: function (args) {
+        'use strict';
+        var index, elementIndex;
+        if (args !== undefined && args.length !== 0) {
+            for (index = 0; index !== args.length; ++index) {
+                elementIndex = this.states.indexOf(args[index]);
+                // only remove those in state list.
+                if (elementIndex !== -1) {
+                    this.states.splice(elementIndex, 1);
+                }
+            }
+        }
+        return this;
+    },
+    on: function (state, event, callback) {
+        'use strict';
+        // first, check number of arguments
+        if (arguments.length === 3) {
+            if (state !== undefined && event !== undefined && callback !== undefined) {
+                if (this.events[state] === undefined) {
+                    this.events[state] = {};
+                }
+                this.events[state][event] = callback;
+            }
+        }
+        return this;
+    },
+    eventTrigger: function (previousState, currentData, previousData) {
+        'use strict';
+        // trigger previous leave event first
+        if (this.events[previousState].leave !== undefined) {
+            this.events[previousState].leave(previousData);
+        }
+        // trigger current enter event
+        if (this.events[this.current].enter !== undefined) {
+            this.events[this.current].enter(currentData);
+        }
+    },
+    forth: function (currentData, previousData) {
+        'use strict';
+        var currentIndex, previousState;
+        currentIndex = this.states.indexOf(this.current);
+        previousState = this.current;
+        // only advance if it is not the last state yet.
+        if (currentIndex !== this.states.length - 1) {
+            this.current = this.states[currentIndex + 1];
+            this.eventTrigger(previousState, currentData, previousData);
+        }
+        return this;
+    },
+    back: function (currentData, previousData) {
+        'use strict';
+        var currentIndex, previousState;
+        currentIndex = this.states.indexOf(this.current);
+        previousState = this.current;
+        // only advance if it is not the first state yet.
+        if (currentIndex !== 0) {
+            this.current = this.states[currentIndex - 1];
+            this.eventTrigger(previousState, currentData, previousData);
+        }
+        return this;
+    },
+    go: function (state, currentData, previousData) {
+        'use strict';
+        var elementIndex, previousState;
+        if (state !== undefined && this.current !== state) {
+            elementIndex = this.states.indexOf(state);
+            // only if the given state is valid
+            if (elementIndex !== -1) {
+                previousState = this.current;
+                this.current = this.states[elementIndex];
+                this.eventTrigger(previousState, currentData, previousData);
+            }
+        }
+        return this;
+    }
+};
+
+module.exports = State;
+
